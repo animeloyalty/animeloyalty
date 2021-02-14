4ac1326434ffd612b2229ba13a3f8ac5801ecc59
Deathspike <meister.deathspike@outlook.com>
2021-02-14 15:22:26 +0100

    Add Windows CI for ffmpeg downloads

--- .travis-wine.sh
+++
@@ -1,6 +0,0 @@
-  docker run --rm \
-    -e GH_TOKEN \
-    -v "${PWD}":/project \
-    -v ~/.cache/electron:/root/.cache/electron \
-    -v ~/.cache/electron-builder:/root/.cache/electron-builder \
-    electronuserland/builder:wine /bin/bash -c "npm ci && npm run release -- --windows"
